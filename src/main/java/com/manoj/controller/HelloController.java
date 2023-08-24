65e671f8e0d1426fbaf2179029fb81676450a042
unknown <manojks.masaurhi@gmail.com>
2023-08-24 16:10:27 +0530

    Added hello controller

---
+++ src/main/java/com/manoj/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.manoj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getMessage()
+	{
+		return "Hello from Application"; 
+	}
+
+}
